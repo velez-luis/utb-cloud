c3a2316f70d0758d7954afbf733fad7f2fd0048f
Luis Velez <luis.velezl@hotmail.com>
2024-10-09 22:24:01 -0500

    Proyecto Base

---
+++ src/test/java/com/cloud/utb/project/JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.utb.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
