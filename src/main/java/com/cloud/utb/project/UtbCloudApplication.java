f25cdc9d9c7d97c457e6c4a3b15f621de36dac3e
Luis Velez <luis.velezl@hotmail.com>
2024-10-09 22:36:09 -0500

    Proyecto Base

--- src/main/java/com/cloud/utb/project/UtbCloudApplication.java
+++ src/main/java/com/cloud/utb/project/UtbCloudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaProjectApplication {
+public class UtbCloudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaProjectApplication.class, args);
+		SpringApplication.run(UtbCloudApplication.class, args);
 	}
 
 }
